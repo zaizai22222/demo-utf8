0863955698bffca64eec7f6cf0cb7ea5ab162ee9
zaizai22222 <qizhang.france@gmail.com>
2016-11-04 10:26:57 +0100

    demo

--- build_db.sql
+++ build_db.sql
@@ -1,9 +1,9 @@
 CREATE DATABSE demo_utf8;
 
-CREATE TABLE IF NOT EXISTS `demo` (
+CREATE TABLE `demo` (
   `id` int(4) NOT NULL AUTO_INCREMENT,
-  `name_latin` char(20) NOT NULL,
+  `name_latin` char(20) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `name_utf8` char(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `name_utf8md4` char(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name_utf8md4` char(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
